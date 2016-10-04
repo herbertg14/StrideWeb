a8b9feff4c51dd0e448f1685cc030ca64e98ee99
herbertg14 <herbert.gutierrez@utexas.edu>
2016-10-04 15:06:38 -0500

    hello world success

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var morgan = require("morgan");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+
+
+app.get("/", function(req, res){
+	res.send("hello world");
+});
+
+app.listen(PORT, function(){
+	console.log("listening on port " + PORT);
+});
